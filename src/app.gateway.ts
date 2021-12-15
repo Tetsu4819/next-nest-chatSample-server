74810d6fc104df2f9aae785907739fc4bed4ecb3
tetsu4819 <tetsu4819@gmail.com>
2021-12-15 16:07:15 +0900

    🔨

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,19 +1,21 @@
-import { Get, Logger } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import {
+  ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  WsResponse,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() server: Server;
   private logger: Logger = new Logger('AppGateway');
 
   afterInit() {
@@ -28,8 +30,33 @@ export class AppGateway
     this.logger.log(`Client disconnected:${client.id}`);
   }
 
+  // @SubscribeMessage('msgToServer')
+  // handleMessage(client: Socket, text: string): WsResponse<string> {
+  //   return { event: 'msgToClient', data: text };
+  // }
+
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, text: string): WsResponse<string> {
-    return { event: 'msgToClient', data: text };
+  handleMessage(
+    @MessageBody() data: { room: string; name: string; message: string },
+  ) {
+    this.logger.log('name is ' + data.name);
+    this.logger.log('msg is ' + data.message);
+    this.server.to(data.room).emit('msgToClient', data);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(
+    @MessageBody() loginInfo: { room: string; name: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    const room = loginInfo.room;
+    const name = loginInfo.name;
+    client.join(room);
+    client.emit('joinedRoom', { room: room, name: name });
+    this.server.to(room).emit('msgToClient', {
+      room: room,
+      name: 'System',
+      message: name + ' entered room!',
+    });
   }
 }
